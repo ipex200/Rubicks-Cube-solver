cd9b606778341131c27dd63c8a0960af2e1155e3
ipex200 <hamalacompt@gmail.com>
2025-02-17 15:37:16 -0700

    Add files via upload

--- learning.py
+++ learning.py
@@ -1,6 +1,10 @@
+def string_to_2d_list(string):
+    words = string.split()  # Split the string into words
+    return [words[i:i+9] for i in range(0, len(words), 9)]
+def list_to_string(two_d_list):
+    return " ".join([item for sublist in two_d_list for item in sublist])
 
 """import the tools that I need """
-import copy
 cube = [
     ['W', 'W', 'W',
      'W', 'W', 'W',
@@ -176,7 +180,8 @@ def move_FF():
     move_F1()
 
 """useful variables"""
-start= copy.deepcopy(cube)
+start= list_to_string(cube)
+start = string_to_2d_list(start)
 pruned = []
 open_state = []
 pre_open = []
@@ -191,7 +196,7 @@ move_set2_undo =[move_U1, move_R1, move_B1, move_L1, move_F1,move_D1]
 continue_algo = True
 continue_iteration = True
 continue_search= True
-
+before={}
 """functions"""
 # calculate the heuristic for the current state of cube
 def heuristic(current, goal):
@@ -200,77 +205,103 @@ def heuristic(current, goal):
         for square_index in range(len(current[face_index])):
             if current[face_index][square_index] != goal[face_index][square_index]:
                 dif += 1
-    return dif / 20
+    return dif / 21
 #function that allows me to print the cube in a bette way
 def print_cube(cube):
     for face in cube:
         for x in range(0,9,3):
             print(face[x:x+3])
         print("")
-
 # calculate the f_score and classify them  for every possible child of the current cube
 def get_f_scores(threshold):
     global  pre_open
     global pruned
     pre_open.clear()
+    if pre_open:
+        previous_move= pre_open[0][0]
+    else:
+        previous_move=" "
+    possibility = [[move_U1,move_U2,move_UU],[move_DD,move_D1,move_D2],[move_B1,move_B2,move_BB],[move_FF,move_F1,move_F2],
+                   [move_LL,move_L2,move_L1],[move_RR,move_R1,move_R2]]
+    possibility= [sublist for sublist in possibility if previous_move not in sublist]
+
     if not open_state:
         g=1
     else:
         g= open_state[0][4]+1
+
     # Appliquer les mouvements de move_set1
     for x in range(len(move_set1)):
-        move_set1[x]()  # Appliquer le mouvement F1, F2, etc.
+        if  any(move_set1[x] in sublist for sublist in possibility) :
+            move_set1[x]()  # Appliquer le mouvement F1, F2, etc.
 
-        # Calculer la fonction de coût f = g + h (heuristique)
-        f = g + heuristic(cube,goal)
-        # Vérifier si on reste sous le seuil
-        if f <= threshold:
-            pre_open.append([move_set1[x],f,heuristic(cube,goal),copy.deepcopy(cube),g])
-        if f > threshold:
-            pruned.append([move_set1[x],f,heuristic(cube,goal),copy.deepcopy(cube),g])
-        # Annuler le mouvement
-        move_set1_undo[x]()
+            # Calculer la fonction de coût f = g + h (heuristique)
+            f = g + heuristic(cube,goal)
+
+            # Vérifier si on reste sous le seuil
+            if f <= threshold:
+                if list_to_string(cube) not in before:
+                    pre_open.append([move_set1[x],f,heuristic(cube,goal),list_to_string(cube),g])
+            if f > threshold:
+                pruned.append([move_set1[x],f,heuristic(cube,goal),list_to_string(cube),g])
+            before.add(list_to_string(cube))
+            # Annuler le mouvement
+            move_set1_undo[x]()
+        else :
+            f = g + heuristic(cube, goal)
+            pruned.append([move_set1[x], f, heuristic(cube, goal), list_to_string(cube), g])
     # Appliquer les mouvements de move_set2
     for x in range(len(move_set2)):
-        # Appliquer le mouvement
-        move_set2[x]()
-        # Calculer la fonction de coût f = g + h (heuristique)
-        f = g + heuristic(cube, goal)
-        # Vérifier si on reste sous le seuil
-        if f <= threshold:
-            pre_open.append([move_set2[x], f, heuristic(cube, goal), copy.deepcopy(cube),g])
-        if f > threshold:
-            pruned.append([move_set2[x], f, heuristic(cube, goal), copy.deepcopy(cube),g])
-        # Annuler le mouvement
-        move_set2_undo[x]()
+        if  any(move_set2[x] in sublist for sublist in possibility) :
+            # Appliquer le mouvement
+            move_set2[x]()
+            # Calculer la fonction de coût f = g + h (heuristique)
+            f = g + heuristic(cube, goal)
+            # Vérifier si on reste sous le seuil
+            if f <= threshold:
+                if list_to_string(cube) not in before:
+                    pre_open.append([move_set2[x], f, heuristic(cube, goal), list_to_string(cube),g])
+            if f > threshold:
+                pruned.append([move_set2[x], f, heuristic(cube, goal), list_to_string(cube),g])
+
+            before.add(list_to_string(cube))
+
+            # Annuler le mouvement
+            move_set2_undo[x]()
+        else:
+            f = g + heuristic(cube, goal)
+            pruned.append([move_set2[x], f, heuristic(cube, goal), list_to_string(cube), g])
     # Trier les résultats pour garder les meilleurs en fonction de f
     pre_open.sort(key=lambda x: (x[1], x[2]))
     return pre_open, pruned
 """zone to scramble the cube with some moves"""
-
 move_D1()
 move_B2()
 move_F2()
 move_D2()
-move_D2()
+move_U1()
 move_L2()
-move_D1()
-start= copy.deepcopy(cube)
+move_F1()
+move_B1()
+move_U1()
+start= list_to_string(cube)
+start= string_to_2d_list(start)
 #lalgo trouve la solution avec f1/F2 / D1/ D2 / R1/R2/L1/L2/ quand on utilise quatre move
 """algorythm"""
 while continue_algo:
     global threshold
+    before = {""}
+
     iteration += 1
     # we handle the case for the first iteration
     if iteration == 1:
         open_state.clear()
-        threshold= heuristic(cube,goal)
+        threshold= 0
         get_f_scores(threshold)
         open_state = pre_open+ open_state
         #we check each node under the threshold
         while open_state:
-
-            cube = copy.deepcopy(open_state[0][3])
+            cube = string_to_2d_list(open_state[0][3])
             # we check if this is the solution and if yes we break the loop
             if cube == goal:
                 continue_algo = 0
@@ -285,19 +316,23 @@ while continue_algo:
         continue
     #now we handle the case for the other iterationw
     if iteration !=1:
-        cube= copy.deepcopy(start)
+        cube=list_to_string(start)
+        cube= string_to_2d_list(cube)
         open_state.clear()
         #now we get the new threshold
         pruned.sort(key=lambda x: (x[1], x[2]))
-        threshold = pruned[0][1]*1.1
+
+        threshold = threshold+ 1
+        print(threshold,"t")
         pruned.clear()
         #now we can get the new openstate
         get_f_scores(threshold)
         open_state = pre_open + open_state
+        print(open_state.__len__())
         #now we can start the loop for the algo
         print(iteration)
         while open_state:
-            cube = copy.deepcopy(open_state[0][3])
+            cube = string_to_2d_list(open_state[0][3])
             # we check if this is the solution and if yes we break the loop
             if cube == goal:
                 continue_algo = 0
@@ -307,6 +342,7 @@ while continue_algo:
             get_f_scores(threshold)
             open_state.pop(0)
             open_state = pre_open + open_state
+
             # in this case if open state is empty the line of code wont just be readed and it will go to the next iteration
 
         continue
